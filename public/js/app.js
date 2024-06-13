2e86eaefd792a45c513f521d795e352975866016
DhyeySonani <sonanidhyey15@gmail.com>
2024-06-13 17:33:11 +0530

    app.js updated

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = '';
 
     // fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-        fetch('http://node3-weather-website-42x9.onrender.com/weather?address=' + location).then((response) => {
+        fetch('https://node3-weather-website-42x9.onrender.com/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
